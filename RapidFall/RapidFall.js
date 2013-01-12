10f486011120836c179ef0f47d2c1d85365dafe9
acasaccia <acasaccia@7826f642-0b08-cb36-82ee-cac5dfa9d2db>
2013-01-12 12:19:09 +0000

    Added Wolf model, refactored RapidFall.update() to hide some members which used to be public.

--- RapidFall/RapidFall.js
+++ RapidFall/RapidFall.js
@@ -1,55 +1,76 @@
 function RapidFall() {
-	
-	this.initializeScene = function () {
 
-		// SCENE
-		this.scene = new THREE.Scene();
-		
-		// CAMERA
-		this.camera = new THREE.PerspectiveCamera(75, 4/3, 0.1, 1000);
-		this.camera.position.z = 5;
-		
-		// RENDERER
-		if( Detector.webgl ){
-			this.renderer = new THREE.WebGLRenderer();
-			this.renderer.setClearColorHex( 0xBBBBBB, 1 );
-		// uncomment if webgl is required
-		//}else{
-			// Detector.addGetWebGLMessage();
-			// return true;
-		}else{
-			this.renderer = new THREE.CanvasRenderer();
+	var spawnPlatformTimer = 0;
+	
+	this.update = function(dt) {
+
+		for(i in this.platforms) {
+			this.platforms[i].gfxObject.translateY(0.01);
+			if (this.platforms[i].gfxObject.position.y > 1) {
+				RapidFall.scene.remove(this.platforms[i].gfxObject);
+				this.platforms.splice(i,1);
+			}
 		}
-		this.renderer.setSize(400, 300);
-		document.body.appendChild(this.renderer.domElement);
 		
-		// LIGHTING
-		var pointLight = new THREE.PointLight(0xFFFFFF);
-		pointLight.position.x = 0;
-		pointLight.position.y = 50;
-		pointLight.position.z = 130;
-		this.scene.add(pointLight);
+		if (RapidFall.Input.isPressed(RapidFall.Config.MOVE_LEFT)) {
+			this.player.gfxObject.translateX(-0.01);
+		}
 		
-	},
+		if (RapidFall.Input.isPressed(RapidFall.Config.MOVE_RIGHT)) {
+			this.player.gfxObject.translateX(0.01);
+		}
 		
-	this.initializeEntities = function () {
-		console.log('initializeEntities');
-	},
+		spawnPlatformTimer += dt;
+		
+		if (spawnPlatformTimer > RapidFall.Config.PLATFORM_SPAWN_INTERVAL) {
+			spawnPlatformTimer = 0;
+			var platform = new RapidFall.GameObjects.Platform();
+			RapidFall.scene.add(platform.gfxObject);
+			this.platforms.push(platform);
+		}
+		
+	}
 	
-	this.spawnPlatformTimer = 0;
-		
-};
+}
+	
+RapidFall.prototype.initializeScene = function () {
 
-// UPDATE FUNCTION
-RapidFall.prototype.update = function(dt) {
+	// SCENE
+	RapidFall.scene = new THREE.Scene();
 	
-	if (RapidFall.Input.isPressed(RapidFall.Config.MOVE_LEFT)) {
-		this.player.gfxObject.translateX(-0.01);
-	}
+	// CAMERA
+	this.camera = new THREE.PerspectiveCamera(30, 4/3, 0.1, 1000);
+	this.camera.position.z = 5;
 	
-	if (RapidFall.Input.isPressed(RapidFall.Config.MOVE_RIGHT)) {
-		this.player.gfxObject.translateX(0.01);
+	// RENDERER
+	if( Detector.webgl ){
+		this.renderer = new THREE.WebGLRenderer();
+		this.renderer.setClearColorHex( 0xBBBBBB, 1 );
+	// uncomment if webgl is required
+	//}else{
+		// Detector.addGetWebGLMessage();
+		// return true;
+	}else{
+		this.renderer = new THREE.CanvasRenderer();
 	}
+	this.renderer.setSize(400, 300);
+	document.body.appendChild(this.renderer.domElement);
+	
+	// LIGHTING
+	var pointLight = new THREE.PointLight(0xFFFFFF);
+	pointLight.position.x = 0;
+	pointLight.position.y = 50;
+	pointLight.position.z = 130;
+	RapidFall.scene.add(pointLight);
+	
+}
+		
+RapidFall.prototype.initializeEntities = function () {
+
+	this.player = new RapidFall.GameObjects.Player();
+	this.platforms = [];
+	var platform = new RapidFall.GameObjects.Platform();
+	this.platforms.push(platform);
 	
 }
 
@@ -68,7 +89,7 @@ RapidFall.prototype.mainloop = function (){
 				accumulator -= RapidFall.Config.PHYSICS_FRAME_TIME;
 		}
 		requestAnimationFrame(mainloop);
-		self.renderer.render(self.scene, self.camera);
+		self.renderer.render(RapidFall.scene, self.camera);
 	})();
 
 }
